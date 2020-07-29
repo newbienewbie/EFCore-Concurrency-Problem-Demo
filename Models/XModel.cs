02a9e9dffc7461ad03decd7615c8fead1dea901f
newbienewbie <itminus@hotmail.com>
2020-07-29 15:21:35 +0800

    use optimistic concurrency token

--- Models/XModel.cs
+++ Models/XModel.cs
@@ -1,9 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace WebApp.Models{
     public class XHouseInventory
     {
         public int Id {get;set;}
         public string Name {get;set;}
         public int Sku{get;set;}
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [Timestamp]
+        public DateTime RowVersion { get; set; }
     }
 
     public class XOrder
